c924bf11f16afb33c5eb72a4ac25b745405b0ba4
TheGrandUtochka <63515107+TheGrandUtochka@users.noreply.github.com>
2022-03-18 12:59:12 +0500

    Update mystery.cpp

--- mystery.cpp
+++ mystery.cpp
@@ -1,16 +1,20 @@
 #include <cmath>
 #include <iostream>
 using namespace std;
-
-int main(){
-	int n;
-	cin >> n;
-	
+ 
+void formula(int n)
+{
 	if (n % 2 != 0 and n >= 3)
 	{
 		cout << pow(n, 2) - (n - 1) * 2;
 	} else {
 		cout << "Wrong number";
 	}
+}
+ 
+int main() {
+	int n;
+	cin >> n;
+	formula(n);
 	return 0;
 }
